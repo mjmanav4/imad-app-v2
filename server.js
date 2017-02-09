25f46e7a6d1f9aa54bc5c3e26d43f3c855bfc444
mjmanav4 <mjmanav4@gmail.com>
2017-02-10 00:38:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('article one is requested and sent');
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-two', function (req, res) {
